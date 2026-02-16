9cb37e49e070d95b8633e62af70f4b5fb71be32e
rasutopi <rasutopi@gmail.com>
2026-02-16 20:21:56 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const proxyRouter1 = require("../proxies/static/v1");
-const proxyRouter2 = require("../proxies/streaming/v2");
+const proxyRouter2 = require("../proxies/streaming/v1");
 
 require('dotenv').config();
 
@@ -471,6 +471,7 @@ app.listen(PORT, '0.0.0.0', () => {
     console.log(`Proxy server running at http://0.0.0.0:${PORT}`);
 
 });
+
 
 
 
